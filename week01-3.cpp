9e1e3b781f9f6ec77f10322ba232ec446cb6cad3
you13750191 <13750191@me.mcu.edu.tw>
2025-09-10 11:55:53 +0800

    week1

---
+++ week01-3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    int ans=0;
+    for(int i=a;i<=b;i++)
+        if(i%3==0ans+=i;
+    )
+    printf("%d",ans);
+    return 0;
+}
---
+++ week01-4a.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    scanf("%d %d",&a,&b);
+    int ans=0;
+    for(int i=a;i<=b;i++){
+        if(i%3==0)ans+=i;
+    }
+    printf("%d",ans);
+
+}
